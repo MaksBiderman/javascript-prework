bc82f977b9bdf1b75de0703650b1d1407f09e006
Maks Biderman <gimm3g0ld@gmail.com>
2023-06-01 11:36:41 +0200

    add variable to js

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,7 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+let computerMove = `kamień`;
+let playerMove = `papier`;
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
