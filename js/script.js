d5be484262c9be819c25fd022d58ddcdb532feb7
Maks Biderman <gimm3g0ld@gmail.com>
2023-06-02 09:34:39 +0200

    add game result

--- js/script.js
+++ js/script.js
@@ -21,3 +21,34 @@ if(playerInput == '2'){
   }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+  }
+if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Przegrywasz!');
+  }
+if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Przegrywasz!');
+  }
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Wygrywasz');
+  }
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis');
+  }
+if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis');
+  }
+if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Przegrywasz');
+  }
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Wygrywasz');
+  }
+else {printMessage('Niedozwolony ruch')
+
+}
